38c2d9c325ce2a5a6d2ad9dd3c5465e59b0a4dad
Nitish <nitish.kr628@gmail.com>
2018-12-08 18:39:46 +0530

    08/12/2018

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Count from './tutorial/Count'
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      count:0
-    }
-  }
+
   render() {
     return (
       <div className="App">
-        <div>
-          <input type="button" value="-" className="button" onClick={()=>this.setState({count:this.state.count-1})}/>
-          <input type="button" value="+" className="button" onClick={()=>this.setState({count:this.state.count+1})}/>
-        </div>
-        <div>
-          <h1>{this.state.count}</h1>
-        </div>
-        
+        <Count />
+        <Count />
       </div>
     );
   }
---
+++ src/tutorial/Count.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props); ///super
+    
+    this.increment=this.increment.bind(this);
+    this.decrement=this.decrement.bind(this);
+  }
+  state = {
+    count:0
+  }
+  increment(){
+    this.setState({
+      count:this.state.count+1,
+    })
+  }
+
+  decrement(){
+    this.setState({
+      count:this.state.count-1,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <input type="button" value="-" 
+            className="button" 
+            onClick={this.decrement}/>
+          <input type="button" value="+" 
+            className="button" 
+            onClick={this.increment}/>
+        </div>
+        <div>
+          <h1>{this.state.count}</h1>
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
